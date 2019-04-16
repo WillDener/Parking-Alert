4f365f62ab597d90e5e305e8cf97f48def4c5bf7
WillDener <wdener8@gmail.com>
2019-04-15 21:22:48 -0300

    first commit

---
+++ src/main/java/com/fatec/parkingalert/ParkingAlert/ParkingAlertApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.parkingalert.ParkingAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingAlertApplication.class, args);
+	}
+
+}
